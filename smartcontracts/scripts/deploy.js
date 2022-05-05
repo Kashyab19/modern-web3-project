f0a9a95af819665004e14d8e05f2ef7dfa63267d
Kashyab19 <kashyabnarayanan3721@gmail.com>
2022-05-05 10:43:15 +0530

    Mid way @ 1hr 31min

---
+++ smartcontracts/scripts/deploy.js
@@ -0,0 +1,24 @@
+const main = async () => {
+  
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
+
+  await transactions.deployed();
+
+  console.log("Transactions Contracts are deployed to:", transactions.address);
+}
+
+const runMain = async () => {
+  
+  try {
+    await main();
+    process.exit(0);
+  } 
+
+  catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+}
+
+runMain();
